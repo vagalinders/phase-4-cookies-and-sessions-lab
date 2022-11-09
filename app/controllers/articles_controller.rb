3c6d7b7dd774e5d0544738c156c6680e52cf98dc
Chris Mann <chrispmann@gmail.com>
2022-11-09 10:12:24 -0800

    commit tree ff69e0cb0534422f4d6316f2b5d3a2b5186a2045

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,27 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+
   def index
-    articles = Article.all.includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+    session[:page_views] ||= 1
+    session[:page_views] += 1
+    if session[:page_views] >= 4
+      render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+    else
+      articles = Article.all.includes(:user).order(created_at: :desc)
+      render json: articles, each_serializer: ArticleListSerializer
+    end
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    if session[:page_views] >= 4
+      render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+    else
+      article = Article.find(params[:id])
+      render json: article
+    end
   end
 
   private
